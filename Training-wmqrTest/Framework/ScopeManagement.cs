95255060d288635f6f6f85cc753b772ee423df40
Lessan Vaezi <lessan@gmail.com>
2012-07-30 09:31:46 +0300

    Added ScopeManagement.cs which was missed in the last commit

---
+++ Training-wmqrTest/Framework/ScopeManagement.cs
@@ -0,0 +1,36 @@
+﻿using Castle.ActiveRecord;
+
+namespace Training_wmqrTest.Framework
+{
+    public static class ScopeManagement
+    {
+        public static SessionScope Scope;
+        public static TransactionScope TransactionScope;
+
+        public static void DisposeScope()
+        {
+            if (TransactionScope != null)
+            {
+                TransactionScope.Dispose();
+                TransactionScope = null;
+            }
+            if (Scope != null)
+            {
+                Scope.Dispose();
+                Scope = null;
+            }
+        }
+
+        public static void CreateScope()
+        {
+            Scope = new SessionScope();
+            TransactionScope = new TransactionScope(OnDispose.Commit);
+        }
+
+        public static void ResetScope()
+        {
+            DisposeScope();
+            CreateScope();
+        }
+    }
+}
